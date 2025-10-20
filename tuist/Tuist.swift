28c1445b6e5d55581f768b4854329f3438334bdf
fortmarek <marekfort@me.com>
2025-10-20 18:17:18 +0200

    Add url to tuist/Tuist.swift

--- tuist/Tuist.swift
+++ tuist/Tuist.swift
@@ -2,6 +2,7 @@ import ProjectDescription
 
 let config = Config(
     fullHandle: "tuist/tuist",
+    url: "https://staging.tuist.dev",
     swiftVersion: .init("5.10"),
     generationOptions: .options(
         optionalAuthentication: true,

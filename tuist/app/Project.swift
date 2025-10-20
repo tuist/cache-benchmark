36dd14d138a4c649e825c8316bf69207c26bb3b1
fortmarek <marekfort@me.com>
2025-10-20 17:58:27 +0200

    Update benchmark with extra code sign flag and macro fingerprint validation skip

--- tuist/app/Project.swift
+++ tuist/app/Project.swift
@@ -23,7 +23,7 @@ func tuistMenuBarDependencies() -> [TargetDependency] {
 let inspectBuildPostAction: ExecutionAction = .executionAction(
     title: "Inspect build",
     scriptText: """
-    eval "$($HOME/.local/bin/mise activate -C $SRCROOT bash --shims)"
+    eval "$($HOME/.local/share/bin/mise activate -C $SRCROOT bash --shims)"
 
     tuist inspect build
     """
